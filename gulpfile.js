33c73f586dad6b490a5d8ae03e3270ae78a062eb
Vladislav Smirnov <vladislav.smirnov@comindware.com>
2015-11-23 09:53:49 +0300

    Version 1.1 release Home page - animation, redesign Webinar welcome page Clean up and fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-var gulp = require('gulp'),
-  uglify = require('gulp-uglify'),
-  gutil = require('gulp-util'),
-  less = require('gulp-less'),
-  path = require('path'),
-  connect = require('gulp-connect'),
-  sourcemaps = require('gulp-sourcemaps'),
-  minifyHTML = require('gulp-minify-html'),
-  imagemin = require('gulp-imagemin'),
-  pngquant = require('imagemin-pngquant'),
-  minifyCss = require('gulp-minify-css');
+var gulp=require('gulp'),
+    uglify=require('gulp-uglify'),
+    gutil=require('gulp-util'),
+    less = require('gulp-less'),
+    path = require('path'),
+    connect = require('gulp-connect'),
+    sourcemaps = require('gulp-sourcemaps'),
+    minifyHTML = require('gulp-minify-html'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    minifyCss = require('gulp-minify-css');
 
 
 gulp.task('connect', function() {
@@ -19,16 +19,16 @@ gulp.task('connect', function() {
 });
 //Uglifies
 
-gulp.task('uglifying', function() {
+gulp.task('uglifying',function(){	
   return gulp.src('utoApp.js')
     .pipe(uglify()).on('error', gutil.log)
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('less', function() {
+gulp.task('less', function () {
   return gulp.src('styles/*.less')
     .pipe(less({
-      paths: [path.join(__dirname, 'less', 'includes')]
+      paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     .pipe(gulp.dest('styles/'))
     .pipe(connect.reload());
@@ -36,42 +36,40 @@ gulp.task('less', function() {
 
 gulp.task('minify-css', function() {
   return gulp.src('styles/utoStyle.css')
-    .pipe(minifyCss()).on('error', gutil.log)
+.pipe(minifyCss()).on('error', gutil.log)  
 
-  .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('styles', function() {
-  console.log('runs styles');
+gulp.task('styles',function(){
+	console.log('runs styles');
 });
 
-gulp.task('imagemin', function() {
-  return gulp.src('photos/*.jpg')
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{
-        removeViewBox: false
-      }],
-      use: [pngquant()]
-    }))
-    .pipe(gulp.dest('dist/images'));
+gulp.task('imagemin',function ()  {
+    return gulp.src('photos/*.jpg')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('minify-html', function() {
   var opts = {
     conditionals: false,
-    spare: false
+    spare:false
   };
-
+ 
   return gulp.src('pages/ru/*.htm')
     .pipe(minifyHTML(opts))
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('utoApp.js', ['uglifying']);
-  gulp.watch('styles/utoStyle.css', ['minify-css']);
-  gulp.watch('pages/ru/*.htm', ['minify-html']);
+gulp.task('watch',function(){
+	gulp.watch('utoApp.js',['uglifying']);
+	gulp.watch('styles/utoStyle.css',['minify-css']);
+    gulp.watch('pages/ru/*.htm',['minify-html']);
 })
 
-gulp.task('default', ['connect', 'uglifying', 'watch', 'minify-css', 'minify-html', 'imagemin']);
+gulp.task('default',['connect','uglifying','watch','minify-css','minify-html','imagemin']);
--- utoApp.js
+++ utoApp.js
@@ -1,60 +1,60 @@
+/*------------------
+//
+//  main app js file
+//
+//---------------*/
 (function() {
+  var tabSelected=1;
 
-var tabSelected=1;
-
-  var app = angular.module('helloPage', ["ngAnimate",'ui.router'])    
+  var app = angular.module('helloPage', ["ngAnimate",'ui.router'])
 
   .config(['$urlRouterProvider','$stateProvider','$animateProvider',function($urlRouterProvider,$stateProvider,$animateProvider){
-$urlRouterProvider.otherwise('mainPage');
+    $urlRouterProvider.otherwise('mainPage');
 
-$stateProvider
-  .state('mainPage',{
-    url:'/mainPage',
-    templateUrl:'pages/ru/mainPage.htm'
-  })
-
-   .state('utoPrograms',{
-    url:'/utoPrograms',
-    templateUrl:'pages/ru/utoPrograms.htm'
-  })
-
-  .state('utoTools',{
-    url:'/utoTools',
-    templateUrl:'pages/ru/utoTools.htm'
-  })
-
-  .state('utoAnswers',{
-    url:'/utoAnswers',
-    templateUrl:'pages/ru/utoAnswers.htm'
-  })
-
-  .state('utoResultsPactis',{
-    url:'/utoResultsPactis',
-    templateUrl:'pages/ru/utoResultsPactis.htm'
-  })
-
-
-  .state('utoResultsOrganis',{
-    url:'/utoResultsOrganis',
-    templateUrl:'pages/ru/utoResultsOrganis.htm'
-  })
-///---страницы на английском
-  .state('utoSkills',{
-    url:'/utoSkills',
-    templateUrl:'pages/ru/utoSkills.htm'
-  })
-
-  .state('utoTrainings',{
-    url:'/utoTrainings',
-    templateUrl:'pages/ru/utoTrainings.htm'
-  })
-
+    $stateProvider
+    .state('mainPage',{
+      url:'/mainPage',
+      templateUrl:'pages/ru/mainPage.htm'
+    })
+    .state('utoPrograms',{
+      url:'/utoPrograms',
+      templateUrl:'pages/ru/utoPrograms.htm'
+    })
+    .state('utoTools',{
+      url:'/utoTools',
+      templateUrl:'pages/ru/utoTools.htm'
+    })
+    .state('utoAnswers',{
+      url:'/utoAnswers',
+      templateUrl:'pages/ru/utoAnswers.htm'
+    })
+    .state('utoResultsPactis',{
+      url:'/utoResultsPactis',
+      templateUrl:'pages/ru/utoResultsPactis.htm'
+    })
+    .state('utoResultsOrganis',{
+      url:'/utoResultsOrganis',
+      templateUrl:'pages/ru/utoResultsOrganis.htm'
+    })
+    ///---страницы на английском
+    .state('utoSkills',{
+      url:'/utoSkills',
+      templateUrl:'pages/ru/utoSkills.htm'
+    })
+    .state('utoTrainings',{
+      url:'/utoTrainings',
+      templateUrl:'pages/ru/utoTrainings.htm'
+    })
+    .state('invitation',{
+      url:'/invitation',
+      templateUrl:'pages/ru/invitation.htm'
+    })
   }]);
 
   app.controller('pageController', function(){
-      tabSelected=5;
+    tabSelected=5;
     checkNavBar();
-  
+
     this.programs = programs;
     this.startWords = startWords;
     this.businesTasks = businesTasks;
@@ -65,192 +65,184 @@ $stateProvider
     this.comments= comments;
   });
 
-function checkNavBar(){
+  function checkNavBar(){
 
-if ($('.navbar').css('opacity')==0&&tabSelected!=1){
- $('.navbar').animate({opacity:'1',top:'0'},600,function(){
-  $('.navbar').css('display','block');
- });
-}else{
-  if (tabSelected==1)
- $('.navbar').animate({opacity:'0',top:'0'},600,function(){
-  $('.navbar').css('display','none');
- });
-}
+    if ($('.navbar').css('opacity')==0&&tabSelected!=1){
+      $('.navbar').animate({opacity:'1',top:'0'},600,function(){
+        $('.navbar').css('display','block');
+      });
+    }else{
+      if (tabSelected==1)
+      $('.navbar').animate({opacity:'0',top:'0'},600,function(){
+        $('.navbar').css('display','none');
+      });
+    }
 
-$('.navbar').height()>56 ?
-  $(".navbar-toggle").trigger( "click" )
-  :null;        
+    $('.navbar').height()>56 ?
+    $(".navbar-toggle").trigger( "click" )
+    :null;
 
-}
+  }
 
-app.controller('basicSkillCtrl',['$scope','$timeout', function($scope,$timeout){
-
-checkNavBar();
-
-var tabSel;
-
-var init = function () {
- 
-  tabSelected=2;
-
-  checkNavBar();
-};
-init();
-
-}]);
-
-
-app.controller('angularCtrl',['$scope','$timeout','$http', function($scope,$timeout,$http){
-
-checkNavBar();
-
-var tabSel;
-
-var init = function () {
- 
-  tabSelected=2;
-
-  checkNavBar();
-};
-
-init();
-
-var containerHeight;
-}]);
-
-app.controller('practiceCtrl',['$scope','$timeout', function($scope,$timeout){
-
-
-checkNavBar();
-
-var tabSel;
-
-var init = function () {
- 
-  tabSelected=2;
-
-  checkNavBar();
-};
-
-init();
-
-var containerHeight;
-}]);
-
-app.controller('TabController', function(){
-
-    this.setTab = function(newValue){
-      this.tab = newValue;
-      tabSelected=newValue;       
-        
+  app.controller('basicSkillCtrl',['$scope','$timeout', function($scope,$timeout){
+    checkNavBar();
+    var tabSel;
+    var init = function () {
+      tabSelected=2;
+      checkNavBar();
     };
-
-    this.isSet = function(tabName){
-
-      return this.tab === tabName;
-     
-    };
-});
-
-  app.controller('landPageCtrl', ['$scope', function($scope,$timeout){
-
-
- TweenLite.set("#centerBlock", {autoAlpha:0, marginBottom:100});
-    angular.element(document).ready(function () {
-
-TweenLite.to("#centerBlock", 0.9, {autoAlpha:1, marginBottom:0, delay:0.5});
-    });
-
-document.getElementById('toMainPart').onclick=function(){
-  $(window).scrollTop($(window).height());
-};
-
-document.getElementById('modalWindowClose').onclick=function(){
-  document.getElementById('modalWindow').style.display='none';
-};
-/*
-document.getElementById('dialogConflict').onclick=function(){
-  document.getElementById('modalWindow').style.display='block';
-
-var secondLeft=$(window).width()/3 -5;
-
-var thirdLeft=($(window).width()/3)*2 - 10;
-
-var timeLine = new TimelineLite();
-
-  timeLine.to(document.getElementById('modalWindow'), 0.3, {opacity:1});
-  timeLine.to($('.modalContainer'), 0, {display:'block'});
-  timeLine.to($('.modalContainer'), 0.3, {top:0});  
-  timeLine.to(document.getElementById('modalContainerSecond'), 0.3, {left:secondLeft});  
-  timeLine.to(document.getElementById('modalContainerThird'), 0.3, {left:thirdLeft},'-=0.3');
-
-  timeLine.play();
-};
-*/
-document.getElementById('modalWindowMore').onclick=function(){
-
-var timeLine = new TimelineLite();
-
-  timeLine.to(document.getElementById('modalContainerFirst'), 0.3, {rotationX:-30});
-  timeLine.to(document.getElementById('modalContainerSecond'), 0.3, {rotationX:-30},'-=0.15');
-  timeLine.to(document.getElementById('modalContainerThird'), 0.3, {rotationX:-30},'-=0.15');
-  timeLine.to($('.modalContainerOverlap'), 0.8, {top:0});
-  timeLine.to($('.modalContainer'), 0, {display:'none'});
-  //timeLine.to(document.getElementById('mcoSecond'), 0.5, {left:secondLeft});  
-  //timeLine.to(document.getElementById('mcoThird'), 0.5, {left:thirdLeft},'-=0.5');
-  timeLine.to(document.getElementById('mcoFirst'), 0.3, {rotationX:0});
-  timeLine.to(document.getElementById('mcoSecond'), 0.3, {rotationX:0},'-=0.15');
-  timeLine.to(document.getElementById('mcoThird'), 0.3, {rotationX:0},'-=0.15');
-
-  timeLine.play();
-};
-
-/*
-$('#landPageBody').scroll(function(){
-  alert($('#landPageBody').scrollTop());
-});
-*/
-$('#slickID').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  adaptiveHeight: false
-});
-
-$('#gallaryId').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  adaptiveHeight: false
-});
-
-
- $(function() {
-    FastClick.attach(document.body);
-});
- 
-$(window).scroll(function(){
-
-//$(window).animate({
-  //  scrollTop: $(window).height()+'px'
- //}, 2000);
-
- //$(window).animate({scrollTop:$(window).height()},600);
-
- // $(window).scrollTop($(window).height());
-});
-tabSelected=1;
-checkNavBar();
+    init();
   }]);
 
-  app.controller('GalleryController', function(){checkNavBar();});
+
+  app.controller('angularCtrl',['$scope','$timeout','$http', function($scope,$timeout,$http){
+    checkNavBar();
+    var tabSel;
+    var init = function () {
+      tabSelected=2;
+      checkNavBar();
+    };
+
+    init();
+  }]);
+
+  app.controller('practiceCtrl',['$scope','$timeout', function($scope,$timeout){
+    checkNavBar();
+    var tabSel;
+    var init = function () {
+      tabSelected=2;
+      checkNavBar();
+    };
+
+    init();
+  }]);
+
+  app.controller('TabController', function(){
+    this.setTab = function(newValue){
+      this.tab = newValue;
+      tabSelected=newValue;
+    };
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
+
+  app.controller('landPageCtrl', ['$scope', function($scope,$timeout){
+    TweenLite.set("#firstCentralBlock", {autoAlpha:0, marginBottom:100});
+
+    angular.element(document).ready(function () {
+      var timeLine = new TimelineLite();
+
+      timeLine.to("#firstCentralBlock", 0.9, {autoAlpha:1, marginBottom:0});
+      timeLine.to("#secondCentralBlock", 0.9, {autoAlpha:1,rotationX:0});
+      timeLine.to("#thirdCentralBlock", 0.9, {autoAlpha:1,rotationX:0},'-=0.9');
+      timeLine.to("#centralHeaderBlocks", 0.9, {autoAlpha:1});
+      timeLine.to("#leftHeaderBlocks", 0.9, {autoAlpha:1});
+      timeLine.to("#rightHeaderBlocks", 0.9, {autoAlpha:1},'-=0.9');
+
+      TweenMax.fromTo(document.getElementById('toMainPart'), 0.7, {
+        boxShadow: "0px 0px 0px 0px rgba(255,255,255,0.3)"
+      }, {
+        boxShadow: "0px 0px 20px 10px rgba(255,255,255,1)",
+        repeat: -1,
+        yoyo: true,
+        ease: Linear.easeNone
+      });
+    });
+
+    document.getElementById('toMainPart').onclick=function(){
+      //  $(window).scrollTop($(window).height());
+      var translate=-$(window).width();
+      var timeLine = new TimelineLite();
+      timeLine.to(document.getElementById('landPageBody'),0, {scale:0.8});
+      timeLine.to(document.getElementById('landingScreen'),0.3, {scale:0.8});
+      timeLine.to(document.getElementById('landingScreen'),0.3, {x:translate});
+      timeLine.to(document.getElementById('landPageBody'),0.5, {scale:1},'-=0.3');
+      timeLine.set(document.getElementById('practice'), {display:'none'});
+      //  document.getElementById('practice').style.display='none'
+    };
+
+    document.getElementById('modalWindowClose').onclick=function(){
+      document.getElementById('modalWindow').style.display='none';
+    };
+    /*
+    document.getElementById('dialogConflict').onclick=function(){
+    document.getElementById('modalWindow').style.display='block';
+
+    var secondLeft=$(window).width()/3 -5;
+
+    var thirdLeft=($(window).width()/3)*2 - 10;
+
+    var timeLine = new TimelineLite();
+
+    timeLine.to(document.getElementById('modalWindow'), 0.3, {opacity:1});
+    timeLine.to($('.modalContainer'), 0, {display:'block'});
+    timeLine.to($('.modalContainer'), 0.3, {top:0});
+    timeLine.to(document.getElementById('modalContainerSecond'), 0.3, {left:secondLeft});
+    timeLine.to(document.getElementById('modalContainerThird'), 0.3, {left:thirdLeft},'-=0.3');
+
+    timeLine.play();
+  };
+  */
+  document.getElementById('modalWindowMore').onclick=function(){
+
+    var timeLine = new TimelineLite();
+
+    timeLine.to(document.getElementById('modalContainerFirst'), 0.3, {rotationX:-30});
+    timeLine.to(document.getElementById('modalContainerSecond'), 0.3, {rotationX:-30},'-=0.15');
+    timeLine.to(document.getElementById('modalContainerThird'), 0.3, {rotationX:-30},'-=0.15');
+    timeLine.to($('.modalContainerOverlap'), 0.8, {top:0});
+    timeLine.to($('.modalContainer'), 0, {display:'none'});
+    //timeLine.to(document.getElementById('mcoSecond'), 0.5, {left:secondLeft});
+    //timeLine.to(document.getElementById('mcoThird'), 0.5, {left:thirdLeft},'-=0.5');
+    timeLine.to(document.getElementById('mcoFirst'), 0.3, {rotationX:0});
+    timeLine.to(document.getElementById('mcoSecond'), 0.3, {rotationX:0},'-=0.15');
+    timeLine.to(document.getElementById('mcoThird'), 0.3, {rotationX:0},'-=0.15');
+
+    timeLine.play();
+  };
 
 
-window.onload = function() {checkNavBar();};
+  $('#landPageBody').scroll(function(){
+    var elementTopOffset=document.getElementById('practicOfApplicationHeader').getBoundingClientRect().top;
+    var windowHeight=$(window).height()/2;
+
+    if (elementTopOffset<windowHeight)  {
+      var timeLine = new TimelineLite(),
+      secondLeft=$(window).width()/3 -5,
+      thirdLeft=($(window).width()/3)*2 - 10;
+
+      timeLine.to(document.getElementById('communicationStepSecond'), 0.6, {left:secondLeft});
+      timeLine.to(document.getElementById('communicationStepThird'), 0.6, {left:thirdLeft},'-=0.6');
+    }
+  });
+
+  $('#slickID').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: false
+  });
+
+  $('#gallaryId').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    adaptiveHeight: false
+  });
+
+  $(function() {
+    FastClick.attach(document.body);
+  });
+  tabSelected=1;
+  checkNavBar();
+}]);
+
+app.controller('sidePageCtrl', function(){
+  tabSelected=1;
+  checkNavBar();
+});
 
 })();
