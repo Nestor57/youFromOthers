e6c5296524125e449ac8c7a652ae8e5c1fa5e045
Vladislav Smirnov <vdmsmirnov@gmail.com>
2016-06-29 13:19:21 +0300

    v0.4 Release. Finish content re-factoring. Style fixes. Bug fixes

--- utoApp.js
+++ utoApp.js
@@ -1,80 +1,70 @@
 /*------------------
-//
 //  main app js file
-//
 //---------------*/
 $(function() {
     function scaleBannerVideoSize(element) {
-        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated')
     }
-
-    $('.carousel').carousel({
-        interval: 2500,
-        wrap: true,
-        keyboard: true
-    })
 
     $('#gallaryId').carousel({
-        interval: 2500,
+        interval: 5000,
         wrap: true,
         keyboard: true
     })
 
-    $('#dialogCarousel').carousel({
-        interval: 2500
+    $('#mainCarousel').carousel({
+        interval: 5000,
+        wrap: true,
+        keyboard: true
     })
+
     if ($('.responsive-calendar').responsiveCalendar) {
-        $('.responsive-calendar').responsiveCalendar();
+        $('.responsive-calendar').responsiveCalendar()
     }
 
-    var from, to, subject, text;
+    var from, to, subject, text
     $('#send_email').click(function() {
-        to = $('#to').val();
-        subject = $('#subject').val();
+        to = $('#to').val()
+        subject = $('#subject').val()
 
-        text = $('#content').val();
-        $('#message').text('Sending E-mail...Please wait');
+        text = $('#content').val()
+        $('#message').text('Sending E-mail...Please wait')
         $.get('http://localhost:3000/send', {
             to: to,
             subject: subject,
             text: text
         }, function(data) {
             if (data == 'sent') {
-                $('#message').empty().html('Email is been sent at ' + to + '.Please check inbox!');
+                $('#message').empty().html('Email is been sent at ' + to + '.Please check inbox!')
             }
-
-        });
-    });
+        })
+    })
 })
 
 $(window).load(function() {
     $('#landingVideo').get(0).play()
-    var corouselHeight = $('.maxHeightDiv').height()
-    $('#mainCarousel').height(corouselHeight)
-      $('#mainCarousel').css({top:($(window).height()/2-corouselHeight/2)})
-    $('#mainCarousel .carousel-inner').height(corouselHeight)
-})
-
-
-$('.toMainPartButton').click(function() {
-    $('.landPageContainer').stop().animate({
-        scrollTop: window.innerHeight
-    }, '300', 'swing')
+        //    var corouselHeight = $('.maxHeightDiv').height()
+        //    $('#mainCarousel').height(corouselHeight)
+        //      $('#mainCarousel').css({top:($(window).height()/2-corouselHeight/2)})
+        //  $('#mainCarousel .carousel-inner').height(corouselHeight)
 })
 
 $('.nextSlideButton').click(function() {
-    var scrollTargetIndex = parseInt(this.id);
-    var scrollTarget = $('#contentBlock' + scrollTargetIndex);
+    var scrollTargetIndex = parseInt(this.id),
+        scrollTarget = $('#contentBlock' + scrollTargetIndex)
+//make performance better pause the carousel
     if (scrollTarget) {
         $('body,html,document').stop().animate({
-            scrollTop: scrollTarget.offset().top
-        }, '150', 'swing');
+            scrollTop: scrollTarget[0].offsetTop
+        }, '150', 'swing')
     }
 })
 
 $('.toMainPartButton').click(function() {
-    var scrollTarget = $('#contentBlock1');
+  //make performance better pause the carousel
+
+    var scrollTarget = $('#contentBlock1')
     $('body,html,document').stop().animate({
         scrollTop: scrollTarget.offset().top
-    }, '150', 'swing');
+    }, '150', 'swing')
 })
