553d183e9c1d3fa76c8a05c38c3b8df9fe586fcf
Vladislav Smirnov <vdmsmirnov@gmail.com>
2017-01-19 16:08:43 +0300

    Found my old project

--- utoApp.js
+++ utoApp.js
@@ -1,11 +1,24 @@
 /*------------------
 //  main app js file
 //---------------*/
+var $ = require("jquery");
+require("bootstrap");
+require("./libs/fastclick.js");
+require("./libs/responsive-calendar/responsive-calendar.css");
+require("./libs/responsive-calendar/responsive-calendar.js");
+require("./node_modules/bootstrap/dist/css/bootstrap.min.css");
+require("./styles/utoStyle.css");
+require("./styles/meStyle.css");
 $(function() {
-    function scaleBannerVideoSize(element) {
-        $('.homepage-hero-module .video-container video').addClass('fadeIn animated')
-    }
-
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function(reg) {
+      // registration worked
+      console.log('Registration succeeded. Scope is ' + reg.scope);
+    }).catch(function(error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+  }
     $('#gallaryId').carousel({
         interval: 5000,
         wrap: true,
@@ -22,7 +35,7 @@ $(function() {
         $('.responsive-calendar').responsiveCalendar()
     }
 
-    var from, to, subject, text
+    var subject, text;
     $('#send_email').click(function() {
         to = $('#to').val()
         subject = $('#subject').val()
@@ -40,19 +53,51 @@ $(function() {
         })
     })
 })
+var continuePlayingCarousels;
+var presentMainPage = function() {
+    var fadeIn = document.querySelector('#firstCentralBlock').animate({
+        opacity: [0, 1]
+    }, 300);
+    fadeIn.onfinish = function() {
+        document.querySelector('#firstCentralBlock').style.opacity = 1;
+        var fadeIn2 = document.querySelector('#secondCentralBlock').animate({
+            opacity: [0, 1]
+        }, 150);
 
-$(window).load(function() {
-    $('#landingVideo').get(0).play()
-        //    var corouselHeight = $('.maxHeightDiv').height()
-        //    $('#mainCarousel').height(corouselHeight)
-        //      $('#mainCarousel').css({top:($(window).height()/2-corouselHeight/2)})
-        //  $('#mainCarousel .carousel-inner').height(corouselHeight)
-})
+        fadeIn2.onfinish = function() {
+            document.querySelector('#secondCentralBlock').style.opacity = 1;
+            var fadeIn3 = document.querySelector('#thirdCentralBlock').animate({
+                opacity: [0, 1]
+            }, 150);
+
+            fadeIn3.onfinish = function() {
+                document.querySelector('#thirdCentralBlock').style.opacity = 1;
+            };
+        };
+    }
+}
+document.onload = function() {
+  presentMainPage();
+
+};
+
+setTimeout(function() {
+        document.querySelector('#firstCentralBlock').style.opacity = 1;
+        document.querySelector('#secondCentralBlock').style.opacity = 1;
+        document.querySelector('#thirdCentralBlock').style.opacity = 1;
+    }, 1500)
+    //document.addEventListener('DOMContentLoaded', function() {
+    // Animate in
+    //    var corouselHeight = $('.maxHeightDiv').height()
+    //    $('#mainCarousel').height(corouselHeight)
+    //      $('#mainCarousel').css({top:($(window).height()/2-corouselHeight/2)})
+    //  $('#mainCarousel .carousel-inner').height(corouselHeight)
+    //})
 
 $('.nextSlideButton').click(function() {
     var scrollTargetIndex = parseInt(this.id),
         scrollTarget = $('#contentBlock' + scrollTargetIndex)
-//make performance better pause the carousel
+
     if (scrollTarget) {
         $('body,html,document').stop().animate({
             scrollTop: scrollTarget[0].offsetTop
@@ -61,10 +106,132 @@ $('.nextSlideButton').click(function() {
 })
 
 $('.toMainPartButton').click(function() {
-  //make performance better pause the carousel
+        var scrollTarget = $('#contentBlock1')
+        $('body,html,document').stop().animate({
+            scrollTop: scrollTarget.offset().top
+        }, '150', 'swing')
+    })
+    //make performance better pause the carousel
+$(window).scroll(function() {
+    $('#gallaryId').carousel('pause')
+    $('#mainCarousel').carousel('pause')
 
-    var scrollTarget = $('#contentBlock1')
-    $('body,html,document').stop().animate({
-        scrollTop: scrollTarget.offset().top
-    }, '150', 'swing')
+    clearTimeout(continuePlayingCarousels);
+    continuePlayingCarousels = setTimeout(function() {
+        $('#gallaryId').carousel('cycle')
+        $('#mainCarousel').carousel('cycle')
+    }, 500)
+})/*------------------
+//  main app js file
+//---------------*/
+var $ = require("jquery");
+require("bootstrap");
+require("./libs/fastclick.js");
+require("./libs/responsive-calendar/responsive-calendar.css");
+require("./libs/responsive-calendar/responsive-calendar.js");
+require("./node_modules/bootstrap/dist/css/bootstrap.min.css");
+require("./styles/utoStyle.css");
+require("./styles/meStyle.css");
+$(function() {
+    $('#gallaryId').carousel({
+        interval: 5000,
+        wrap: true,
+        keyboard: true
+    })
+
+    $('#mainCarousel').carousel({
+        interval: 5000,
+        wrap: true,
+        keyboard: true
+    })
+
+    if ($('.responsive-calendar').responsiveCalendar) {
+        $('.responsive-calendar').responsiveCalendar()
+    }
+
+    var subject, text;
+    $('#send_email').click(function() {
+        to = $('#to').val()
+        subject = $('#subject').val()
+
+        text = $('#content').val()
+        $('#message').text('Sending E-mail...Please wait')
+        $.get('http://localhost:3000/send', {
+            to: to,
+            subject: subject,
+            text: text
+        }, function(data) {
+            if (data == 'sent') {
+                $('#message').empty().html('Email is been sent at ' + to + '.Please check inbox!')
+            }
+        })
+    })
+})
+var continuePlayingCarousels;
+var presentMainPage = function() {
+    var fadeIn = document.querySelector('#firstCentralBlock').animate({
+        opacity: [0, 1]
+    }, 300);
+    fadeIn.onfinish = function() {
+        document.querySelector('#firstCentralBlock').style.opacity = 1;
+        var fadeIn2 = document.querySelector('#secondCentralBlock').animate({
+            opacity: [0, 1]
+        }, 150);
+
+        fadeIn2.onfinish = function() {
+            document.querySelector('#secondCentralBlock').style.opacity = 1;
+            var fadeIn3 = document.querySelector('#thirdCentralBlock').animate({
+                opacity: [0, 1]
+            }, 150);
+
+            fadeIn3.onfinish = function() {
+                document.querySelector('#thirdCentralBlock').style.opacity = 1;
+            };
+        };
+    }
+}
+window.onload = function() {
+
+};
+
+setTimeout(function() {
+        document.querySelector('#firstCentralBlock').style.opacity = 1;
+        document.querySelector('#secondCentralBlock').style.opacity = 1;
+        document.querySelector('#thirdCentralBlock').style.opacity = 1;
+    }, 1500)
+    //document.addEventListener('DOMContentLoaded', function() {
+    // Animate in
+    //    var corouselHeight = $('.maxHeightDiv').height()
+    //    $('#mainCarousel').height(corouselHeight)
+    //      $('#mainCarousel').css({top:($(window).height()/2-corouselHeight/2)})
+    //  $('#mainCarousel .carousel-inner').height(corouselHeight)
+    //})
+
+$('.nextSlideButton').click(function() {
+    var scrollTargetIndex = parseInt(this.id),
+        scrollTarget = $('#contentBlock' + scrollTargetIndex)
+
+    if (scrollTarget) {
+        $('body,html,document').stop().animate({
+            scrollTop: scrollTarget[0].offsetTop
+        }, '150', 'swing')
+    }
+})
+
+$('.toMainPartButton').click(function() {
+        var scrollTarget = $('#contentBlock1')
+        $('body,html,document').stop().animate({
+            scrollTop: scrollTarget.offset().top
+        }, '150', 'swing')
+    })
+    //make performance better pause the carousel
+$(window).scroll(function() {
+    $('#gallaryId').carousel('pause')
+    $('#mainCarousel').carousel('pause')
+
+    clearTimeout(continuePlayingCarousels);
+    continuePlayingCarousels = setTimeout(function() {
+        $('#gallaryId').carousel('cycle')
+        $('#mainCarousel').carousel('cycle')
+    }, 500)
 })
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+var path = require('path');
+
+var AggressiveMergingPlugin = require('webpack/lib/optimize/AggressiveMergingPlugin');
+var LimitChunkCountPlugin = require('webpack/lib/optimize/LimitChunkCountPlugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var webpack = require('webpack');
+module.exports = {
+    entry: {
+        js: "./utoApp.js",
+    },
+    output: {
+        path: __dirname,
+        filename: "bundle.js",
+        publicPath:'./'
+    },
+    resolve: {
+        extensions: ['', '.js']
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+        }, {
+            test: require.resolve("jQuery"),
+            loader: "imports?this=>window"
+        }, {
+            test: /\.(jpg|png)$/,
+            loader: "file-loader"
+        }, {
+            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "file-loader"
+        },{
+        test: /\.html$/,
+        loader: "html"
+      }]
+    },
+    plugins: [
+        new LimitChunkCountPlugin({
+            maxChunks: 1
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+              sequences:true,
+              properties:true,
+              dead_code:true,
+              drop_debugger:false,
+              conditionals:true,
+              booleans:true,
+              comparisons:true,
+              if_return:true,
+              join_vars:true,
+              cascade:true,
+              warnings:false,
+              keep_fargs:false,
+              passes:2
+            },
+            output: {
+              comments: false
+            },
+            sourceMap:true
+        }),
+        new AggressiveMergingPlugin({
+            minSizeReduce: 1.5,
+            moveToParents: true
+        }),
+        new ExtractTextPlugin('/bundle.css'),
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            filename: 'home.htm',
+            template: './mainPage.htm',
+            minify: {
+              collapseWhitespace:true,
+              conservativeCollapse: true,
+              minifyCSS: true,
+              minifyURLs: true,
+              removeComments: true,
+              removeEmptyAttributes: true,
+            //  removeEmptyElements:true,
+              removeRedundantAttributes: true
+            }
+        })
+    ],
+    root: [
+        path.resolve(__dirname + '/'),
+    ]
+};
